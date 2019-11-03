0f67ae3e690fbd0383a81628a9d0ccf2243f0bb3
unicornmeows <57209097+unicornmeows@users.noreply.github.com>
2019-11-03 15:23:15 +0800

    Update register.php

--- project/register.php
+++ project/register.php
@@ -1,22 +1,20 @@
-   <style type="text/css">
+  <style type="text/css">
         * { 
             padding:0; 
             margin:0; 
         } 
         body {
             margin: 0px;
-            background-color: white;
+            background-color: #FAEEE3;
         }
         .top {
-            position: fixed;
+            position: absolute;
             background-color: white;
             top:0;
             width: 100%;
             height: 40px;
             z-index: 3;
-
         }
-
         .top a {
             text-decoration: none;
             color: #A9A9A9;
@@ -26,10 +24,11 @@
             color: black;
             font-weight: bold;
         }
-        .rightM {
+         .rightM {
             position:absolute;
             right: 70px;
-            top: 5px;
+            bottom: 5px;
+            font-family: 'Playfair Display SC', serif;
         }
         .leftM {
             position: absolute;
@@ -38,12 +37,11 @@
             padding-right: 20px;
             border-right: 1px solid grey;
         }
-
         .main {
-            width: 90%;
+            width: 100%;
             background-color: #FAEEE3;
             margin:auto;
-            height: 800px;
+            height: 900px;
         }
         body,input{
             margin: 0;
@@ -55,10 +53,11 @@
         }
         .container{
             width: 100%;
+            padding-top: 50px;
         }
         .register-box{
             position: relative;
-            height: 600px;
+            height: 850px;
             width: 800px;
             top: 50px;
             margin: 0 auto;
@@ -75,26 +74,27 @@
             text-align: center;
             font-size: 28px;
             font-weight: 800;
-            color: #ff5000;
+            color: black;
             line-height: 50px;
+             font-family: 'Playfair Display SC', serif;
         }
         form {
             position: relative;
             top:50px;
             left:150px;
-
         }
         .box{
             width: 420px;
             height: 100px;
-            font-weight: 700;
+            font-weight: 550; 
+            font-size: 15px;
+            font-family: 'Playfair Display SC', serif;
         }
         .input-field{
             display: inline-block;
             position: relative;
             left: 50px;
             top:10px;
-
             /*background: green;*/
         }
         .input-field input {   
@@ -103,14 +103,12 @@
             border: 2px solid #ccc;
             border-radius: 5px;
         }
-
-
         #submit-button{
             display: inline-block;
             width: 80px;
             height: 40px;
             border-radius: 5px;
-            background: red;
+            background: white;
             position: relative;
             left: 200px;
         }
@@ -132,9 +130,12 @@
  
     </style>
 
-<body style="background: white;">
+<header style="background: white;">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display+SC&display=swap" rel="stylesheet">
 <div class="top">
-    <div class="leftM"><strong style="font: italic 1.5em Georgia, serif;"><a href='home.php' style='text-decoration: none;color: black'>F&F Designer Bags</a></strong></div>
+    <div class="leftM">
+        <a href='home.php'><img height="30px" src="logo.jpg"></a>
+    </div>
     <div class="rightM">
     <?php 
     session_start();
@@ -143,16 +144,16 @@
     <a href="login.php">Login&nbsp;&nbsp;</a>';
     }
     else {
-        echo '<div style="color:#A9A9A9;display:inline-block;">Welcome,'.$_SESSION['user'].' &nbsp;&nbsp;</div>';
+        echo '<a href="myAccount.php">Account&nbsp;&nbsp;</a>';
         echo '<a href="logoff.php">Logoff&nbsp;&nbsp;</a>';
     }
-
     ?>
-    <a href="myAccount.php">Account&nbsp;&nbsp;</a>
     <a href="cart.php">Cart</a>
     </div> 
 </div>
+</header>
 
+<body>
 <div class="main">
 <div class="container">
     <div class="register-box">
@@ -160,13 +161,33 @@
             <span>Registration</span>
         </div>
         <form action="doRegister.php" method="post" onsubmit="return check()">
+
             <div class="box">
                 <span style="color: red;">*</span>
-                <label for="username">Username</label><br>
+                <label for="firstname">First Name</label><br>
                 <div class="input-field">
-                    <input type="text" id="username" name="username" placeholder=" Please enter your username" required />
+                    <input type="text" id="firstname" name="firstname" placeholder=" Please enter your first name" required />
                 </div>
             </div>
+
+            <div class="box">
+                <span style="color: red;">*</span>
+                <label for="username">Last Name</label><br>
+                <div class="input-field">
+                    <input type="text" id="username" name="username" placeholder=" Please enter your last name" required />
+                </div>
+            </div>
+
+            <div class="box">
+                <span style="color: red;">*</span>
+                <label for="gender">Gender</label><br>
+                <div class="input-field">
+                    <input type="radio" style="width:10px; height: 10px;" id="gender" name="gender" value="male" required />Male &nbsp&nbsp&nbsp&nbsp
+                    <input type="radio" style="width:10px; height: 10px;" id="gender" name="gender" value="female" />Female &nbsp&nbsp&nbsp&nbsp
+                    <input type="radio" style="width:10px; height: 10px;" id="gender" name="gender" value="other" />Other
+                </div>
+            </div>
+
 
             <div class="box">
                 <span style="color: red;">*</span>
@@ -208,16 +229,17 @@
         </form>
     </div>
 </div>
+</div>
+</body>
 
 <div class="content">
 <footer>
     <div id="footer">
     <i>Copyright &copy; 2019 F&F Designer Bags<br>
-    <a href="mailto:shihao@feng.com">shihao@feng.com</i></a>
+    <a href="mailto:shihao@feng.com">shihao@feng.com</a>
+        <a href="mailto:fannyee@liow.com">fannyee@liow.com</i></a>
     </div>
 </footer>
-</div>
-</div>
 
 <script type="text/javascript">
     function check() {
@@ -231,7 +253,6 @@
             pass.focus();
             return false;
         }
-
         var reg = /^[0-9]+$/;
         var result = reg.test(phone.value);
         if(!result) {
@@ -241,6 +262,5 @@
             return false;
         }
     }
-
 </script>
 </body>
